48f0749cdade0c05ac039b42f7fe6253d2bac23c
Jake <jakereh91@gmail.com>
2021-03-18 10:08:08 +0100

    add navigation

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,11 @@
-import React from "react";
-import { StyleSheet, View } from "react-native";
-import { Home, Game } from '@screens';
-import { AppLoading } from 'expo'
-import { useFonts, DeliusUnicase_400Regular, DeliusUnicase_700Bold } from '@expo-google-fonts/delius-unicase';
-import { Text } from '@components'
+import React, { ReactElement } from "react";
+import { AppBootstrap } from '@components';
+import Navigator from '@config/navigator';
 
-export default function App() {
-  const [ fontLoaded ] = useFonts({ DeliusUnicase_400Regular, DeliusUnicase_700Bold })
-
-  if(!fontLoaded) return <AppLoading />;
-
+export default function App(): ReactElement {
   return (
-    <View style={styles.container}>
-      <Text onPress={() => {alert(true)}} style={{ fontSize: 24 }} weight="400">{'Hello'}</Text>
-    </View>
+    <AppBootstrap>
+      <Navigator />
+    </AppBootstrap>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
--- src/screens/home/home.tsx
+++ src/screens/home/home.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { ReactElement } from 'react'
+import { ScrollView, Text, Button } from 'react-native'
 import styles from './home.styles';
+import { StackNavigationProp } from '@react-navigation/stack'
+import { StackNavigatorParams } from '@config/navigator'
 
-export default function Home() {
+type HomeProps = {
+  navigation: StackNavigationProp<StackNavigatorParams, "Home">
+}
+
+export default function Home({ navigation }: HomeProps): ReactElement {
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <Text>Home</Text>
-    </View>
+      <Button 
+        title="Game"
+        onPress={() => {
+          navigation.navigate("Game", { gameId: "455"})
+        }}
+      />
+    </ScrollView>
   )
 }
