acdedfb0715a17be02fe975d05d0c634067a2420
Jake Hill <jakereh91@gmail.com>
2021-03-29 14:49:12 +0200

    extract sound logic into hook, add end game line animation

--- src/components/board/board.tsx
+++ src/components/board/board.tsx
@@ -1,48 +1,65 @@
-import React, { ReactElement } from 'react'
-import { View, TouchableOpacity } from 'react-native'
-import Text from '../text/text'
-import { BoardState } from '@utils'
+import React, { ReactElement } from "react";
+import { View, TouchableOpacity } from "react-native";
+import Text from "../text/text";
+import { BoardState, BoardResult } from "@utils";
+import BoardLine from "./board-line";
 
 type BoardProps = {
-  state: BoardState,
-  size: number,
-  onCellPressed?: (index: number) => void
-  disabled: boolean
-}
+  state: BoardState;
+  size: number;
+  onCellPressed?: (index: number) => void;
+  disabled?: boolean;
+  gameResult?: BoardResult | false;
+};
 
-export default function Board({ state, disabled, size, onCellPressed }: BoardProps): ReactElement {
+export default function Board({
+  state,
+  disabled,
+  size,
+  gameResult,
+  onCellPressed,
+}: BoardProps): ReactElement {
   return (
-    <View style={{ 
-      width: size, 
-      height: size, 
-      backgroundColor: 'green',
-      flexDirection: 'row',
-      flexWrap: 'wrap'
-    }}>
+    <View
+      style={{
+        width: size,
+        height: size,
+        backgroundColor: "green",
+        flexDirection: "row",
+        flexWrap: "wrap",
+      }}
+    >
       {state.map((cell, index) => {
         return (
-          <TouchableOpacity 
+          <TouchableOpacity
             onPress={() => onCellPressed && onCellPressed(index)}
             disabled={cell !== null || disabled}
             style={{
-              width: '33.33333%',
-              height: '33.33333%',
-              backgroundColor: 'white',
+              width: "33.33333%",
+              height: "33.33333%",
+              backgroundColor: "white",
               borderWidth: 1,
-              alignItems: 'center',
-              justifyContent: 'center'
-            }} 
+              alignItems: "center",
+              justifyContent: "center",
+            }}
             key={`cell-${index}`}
           >
-            <Text style={{
-                fontSize: size / 8
+            <Text
+              style={{
+                fontSize: size / 8,
               }}
             >
               {cell}
             </Text>
           </TouchableOpacity>
-        )
+        );
       })}
+      {true && (
+        <BoardLine
+          size={size}
+          gameResult={{ winner: "o", diagonal: "MAIN", direction: "D" }}
+        />
+      )}
     </View>
-  )
+  );
 }
--- src/screens/single-player-game/single-player-game.tsx
+++ src/screens/single-player-game/single-player-game.tsx
@@ -1,54 +1,88 @@
-import React, { useState, useEffect, ReactElement, useRef } from 'react'
-import { SafeAreaView } from 'react-native'
-import styles from './single-player-game.styles';
-import { GradientBackground, Board } from '@components'
-import { isEmpty, BoardState, isTerminal, getBestMove } from '@utils'
-import { Audio } from 'expo-av';
-import * as Haptics from 'expo-haptics';
-
+import React, { useState, useEffect, ReactElement } from "react";
+import { SafeAreaView } from "react-native";
+import styles from "./single-player-game.styles";
+import { GradientBackground, Board } from "@components";
+import {
+  isEmpty,
+  BoardState,
+  isTerminal,
+  getBestMove,
+  Cell,
+  useSounds,
+} from "@utils";
 
 export default function Game(): ReactElement {
   const [boardState, setBoardState] = useState<BoardState>([
-    null,null,null,
-    null,null,null,
-    null,null,null
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
   ]);
-  const [turn, setTurn] = useState<"HUMAN" | "BOT">(Math.random() < 0.5 ? "HUMAN" : "BOT")
+  const [turn, setTurn] = useState<"HUMAN" | "BOT">(
+    Math.random() < 0.5 ? "HUMAN" : "BOT"
+  );
   const [isHumanMaximising, setIsHumanMaximising] = useState<boolean>(true);
-  const pop1SoundRef = useRef<Audio.Sound | null>(null);
-  const pop2SoundRef = useRef<Audio.Sound | null>(null);
-
+  const playSound = useSounds();
   const gameResult = isTerminal(boardState);
 
   const insertCell = (cell: number, symbol: "x" | "o"): void => {
     const stateCopy: BoardState = [...boardState];
-    if(stateCopy[cell] || isTerminal(stateCopy)) return;
-    
-    stateCopy[cell] = symbol
-    setBoardState(stateCopy)
+    if (stateCopy[cell] || isTerminal(stateCopy)) return;
+
+    stateCopy[cell] = symbol;
+    setBoardState(stateCopy);
     try {
-      symbol === 'x' ? pop1SoundRef.current?.replayAsync() : pop1SoundRef.current?.replayAsync();
-      Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light)
-    } catch(error) {
+      symbol === "x" ? playSound("pop1") : playSound("pop2");
+    } catch (error) {
       console.log(error);
     }
-  }
+  };
 
   const handleOnCellPressed = (cell: number): void => {
     if (turn !== "HUMAN") return;
-    insertCell(cell, isHumanMaximising ? 'x' : "o")
-    setTurn("BOT")
-  }
+    insertCell(cell, isHumanMaximising ? "x" : "o");
+    setTurn("BOT");
+  };
+
+  const getWinner = (winnerSymbol: Cell): "HUMAN" | "BOT" | "DRAW" => {
+    if (winnerSymbol === "x") {
+      return isHumanMaximising ? "HUMAN" : "BOT";
+    }
+    if (winnerSymbol === "o") {
+      return !isHumanMaximising ? "HUMAN" : "BOT";
+    }
+
+    return "DRAW";
+  };
 
   useEffect(() => {
     if (gameResult) {
-      // handle game finished
-      alert("Game Over")
+      const winner = getWinner(gameResult.winner);
+      if (winner === "HUMAN") {
+        playSound("win");
+        alert("You Won!");
+      }
+      if (winner === "BOT") {
+        playSound("loss");
+        alert("You Lost!");
+      }
+      if (winner === "DRAW") {
+        playSound("draw");
+        alert("It's a draw!");
+      }
     } else {
       if (turn === "BOT") {
         if (isEmpty(boardState)) {
           const centreAndCorners = [0, 2, 4, 6, 8];
-          const firstMove = centreAndCorners[Math.floor(Math.random() * centreAndCorners.length)];
+          const firstMove =
+            centreAndCorners[
+              Math.floor(Math.random() * centreAndCorners.length)
+            ];
           insertCell(firstMove, "x");
           setIsHumanMaximising(false);
           setTurn("HUMAN");
@@ -61,39 +95,19 @@ export default function Game(): ReactElement {
     }
   }, [boardState, turn]);
 
-  useEffect(() => {
-    // Load sounds
-    const pop1SoundObject = new Audio.Sound();
-    const pop2SoundObject = new Audio.Sound();
-    const loadSounds = async () => {
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-      await pop1SoundObject.loadAsync(require("@assets/pop_1.wav"));
-      pop1SoundRef.current = pop1SoundObject;
-
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    await pop2SoundObject.loadAsync(require("@assets/pop_2.wav"));
-    pop2SoundRef.current = pop2SoundObject;
-    }
-    loadSounds();
-
-    return () => {
-      // Unload sounds
-      pop1SoundObject && pop1SoundObject.unloadAsync();
-    }
-  }, [])
-
   return (
     <GradientBackground>
-       <SafeAreaView style={styles.container}>
+      <SafeAreaView style={styles.container}>
         <Board
           disabled={Boolean(isTerminal(boardState)) || turn !== "HUMAN"}
-          onCellPressed={cell => {
-            handleOnCellPressed(cell)
+          onCellPressed={(cell) => {
+            handleOnCellPressed(cell);
           }}
-          state={boardState} 
-          size={300}/>
+          state={boardState}
+          size={300}
+          gameResult={gameResult}
+        />
       </SafeAreaView>
     </GradientBackground>
-   
-  )
+  );
 }
